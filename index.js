37df841aa90491fea2256b658d895e59c0aeb4dd
unknown <ahmedhazm41@gmail.com>
2025-01-25 17:32:12 +0300

    13

--- index.js
+++ index.js
@@ -55,7 +55,10 @@ const User = mongoose.model("User", userSchema);
 const Task = mongoose.model("Task", taskSchema);
 
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
